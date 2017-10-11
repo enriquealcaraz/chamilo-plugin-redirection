b46308530e4176fa6d524df23f021e24f1096b7a
Enrique Alcaraz <32360461+enriquealcaraz@users.noreply.github.com>
2017-10-11 10:40:04 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
-<?php
+<<?php
 /* For licensing terms, see /license.txt */
-
 /**
  * Config the plugin
  * @author Enrique Alcaraz Lopez
  * @package chamilo.plugin.redirection
  */
-
 require_once __DIR__.'/config.php';
-
 api_protect_admin_script();
-
 $list = RedirectionPlugin::getAll();
-
 if (isset($_REQUEST['id'])) {
     RedirectionPlugin::delete($_REQUEST['id']);
     Display::addFlash(Display::return_message(get_lang('Deleted')));
@@ -28,46 +23,48 @@ if (isset($_REQUEST['id'])) {
     header('Location: index.php');
     exit;
 }
-
-Display::display_header();
-
-?>
+$content = '
 <form action="./index.php" method="post">
     <div class="table-responsive well">
         <table class="table table-condensed">
             <thead>
             <td><input type="text" class="form-control" placeholder="User Id" name="user_id"/></td>
             <td><input type="text" class="form-control" placeholder="URL" name="url"/></td>
-            <td><input type='submit' value='Add' name="submit_button" class='btn btn-primary'/></td>
+            <td><input type=\'submit\' value=\'Add\' name="submit_button" class=\'btn btn-primary\'/></td>
             </thead>
         </table>
     </div>
 </form>
-
-
 <div class="table-responsive">
     <table class="table table-bordered table-condensed">
         <tr>
             <th>User</th>
             <th>URL</th>
             <th></th>
-        </tr>
-        <?php
-        foreach ($list as $item) {
-            $userInfo = api_get_user_info($item['user_id']);
-            $userName = get_lang('Unknown');
-            if (!empty($userInfo)) {
-                $userName = $userInfo['complete_name_with_username'].' - '.$item['user_id'];
-            }
-            echo '<tr>';
-            echo '<td>'.$userName.'</td>';
-            echo '<td>'.$item['url'].'</td>';
-            echo '<td><a class="btn btn-danger" href="index.php?id='.$item['id'].'">Delete</a></td>';
-            echo '</tr>';
-        }
-        ?>
-    </table>
-</div>
-<?php
-
-Display::display_footer();
+        </tr>        
+';
+foreach ($list as $item) {
+    $userInfo = api_get_user_info($item['user_id']);
+    $userName = get_lang('Unknown');
+    if (!empty($userInfo)) {
+        $userName = $userInfo['complete_name_with_username'].' - '.$item['user_id'];
+    }
+    $content.= '<tr>';
+    $content.= '<td>'.$userName.'</td>';
+    $content.= '<td>'.$item['url'].'</td>';
+    $content.= '<td><a class="btn btn-danger" href="index.php?id='.$item['id'].'">Delete</a></td>';
+    $content.= '</tr>';
+}
+$content.= '
+</table>
+</div>';
+$tpl = new Template(
+    '',
+    true,
+    true,
+    false,
+    false,
+    false
+);
+$tpl->assign('content', $content);
+$tpl->display_one_col_template();
